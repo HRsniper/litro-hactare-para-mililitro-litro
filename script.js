90b5b31bac53a02407daad30ff4518ea6bb99947
Hércules Ramlow <63937242+HRsniper@users.noreply.github.com>
2022-03-30 17:31:59 -0400

    end v2

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ console.log({ mlB: mlB + "mL/Bomba" });
 const mll = mlB / vB; // ml
 
 console.log({ mll: mll + "mL/L" });
+
+const doselhaId = document.getElementById("doselha");
+const volumelhaId = document.getElementById("volumelha");
+const volumeId = document.getElementById("volume");
+const resultId = document.getElementById("result");
+
+function handlerOnSubmit(event) {
+  event?.preventDefault();
+  console.log("Formulário enviado");
+  calc(doselhaId.value, volumelhaId.value, volumeId.value);
+}
+
+function calc(doselha, volumelha, volume) {
+  const D = ((doselha / volumelha) * volume).toFixed(1); // l
+  const Dml = (D * 1000).toFixed(1); // ml
+
+  resultId.innerHTML = `${D}L | ${Dml}mL de produto/veneno para ${volume}L de água`;
+
+  console.log({ D: D + "L", Dml: Dml + "mL" });
+  return { D, Dml };
+}
